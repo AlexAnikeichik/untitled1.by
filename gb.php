0aa7aaf3a3a92601eff134515ddb503cdb215139
Алексей <anikeichik@tut.by>
2018-07-23 16:36:07 +0300

    7777777555552

---
+++ gb.php
@@ -0,0 +1,15 @@
+<?php echo "Hello world"
+/*
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 23.07.2018
+ * Time: 11:29
+*/
+?>
+<!--
+* Created by PhpStorm.
+* User: Алексей
+* Date: 23.07.2018
+* Time: 11:29
+-->
+"Good Bay!!!!!!!!!!";
